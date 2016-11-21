f76bbf7eba74ab6b9afac83ae208fc889a511197
junior-upgi <junior-upgi@users.noreply.github.com>
2016-11-21 12:55:26 +0800

    implement broadcast schedule (#1)
    implement a make shift broadcast form

--- broadcast.js
+++ broadcast.js
@@ -2,118 +2,106 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var moment = require("moment-timezone");
-var mssql = require("mssql");
 var CronJob = require("cron").CronJob;
-
+var mssql = require("mssql");
+var httpRequest = require("request");
 var config = require("./config.js");
+var telegram = require("./model/telegram.js");
 
 var app = express();
+app.set("view engine", "ejs");
 app.use(morgan("dev")); // log request and result to console
-app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.use(bodyParser.json()); // parse application/json
 var jsonParser = bodyParser.json();
 
-// initialize application's data model
-var botList = [],
-    chatList = [],
-    userList = [],
-    messageQueue = [];
-mssql.connect(config.mssqlConfig).then(function() { // establish server connection
-    var mssqlRequest = new mssql.Request(); // initialize a request object
-    var queryString = "SELECT * FROM telegram.dbo.bot;";
-    mssqlRequest.query(queryString)
-        .then(function(resultset) { //query for a list of telegram bots available
-            mssql.close();
-            console.log("推播機器人列表查詢成功");
-            botList = resultset;
-        }).catch(function(error) {
-            console.log("推播機器人列表查詢失敗：" + error);
-            botList = [];
-        });
-    queryString = "SELECT * FROM telegram.dbo.chat;";
-    mssqlRequest.query(queryString)
-        .then(function(resultset) { //query for a list of chat rooms available
-            mssql.close();
-            console.log("聊天室列表查詢成功");
-            chatList = resultset;
-        }).catch(function(error) {
-            console.log("聊天室列表查詢失敗：" + error);
-            chatList = [];
-        });
-    queryString =
-        "SELECT a.*,b.NAME,b.DEP,c.NAME AS DEP_NAME,b.POS_NAME,b.ID_NO,b.CNT_TEL2 " +
-        "FROM telegram.dbo.[user] a " +
-        "LEFT JOIN DB_U105.dbo.MF_YG b ON a.erpID=b.YG_NO " +
-        "LEFT JOIN DB_U105.dbo.DEPT c ON b.DEP=c.DEP " +
-        "WHERE b.OUT_DAY IS NULL AND b.YG_NO IS NOT NULL;";
-    mssqlRequest.query(queryString)
-        .then(function(resultset) { //query for a list of users available
-            mssql.close();
-            console.log("使用者列表查詢成功");
-            userList = resultset;
-        }).catch(function(error) {
-            console.log("使用者列表查詢失敗：" + error);
-            userList = [];
-        });
-});
+var messageQueue = []; // array to hold message queue
 
-app.route("/broadcast/json") // routes related to broadcasting message passed in through JSON
+app.route("/broadcast") // routes related to broadcasting message passed in through JSON
     .get(function(request, response) { // supply user with a broadcast form
-        return response.status(200).render("jsonForm");
+        var mssqlConnection = new mssql.Connection(config.mssqlConfig); // init db connection obj
+        var mssqlRequest;
+        mssqlConnection.connect() // start db connection
+            .then(function() { // connect success
+                mssqlRequest = new mssql.Request(mssqlConnection); // init db request obj
+                var botList = [];
+                var chatList = [];
+                var userList = [];
+                var getTelegramDataList = function(queryString, mssqlRequestObject) {
+                    return mssqlRequestObject.query(queryString);
+                };
+                getTelegramDataList(telegram.getBotListQuery, mssqlRequest) // get bot data
+                    .then(function(resultset) {
+                        botList = resultset;
+                        return getTelegramDataList(telegram.getChatListQuery, mssqlRequest); // get chat data
+                    }).then(function(resultset) {
+                        chatList = resultset;
+                        return getTelegramDataList(telegram.getUserListQuery, mssqlRequest); // get user data
+                    }).then(function(resultset) {
+                        userList = resultset;
+                        mssqlConnection.close(); // close database connection
+                        console.log("Telegram 資料查詢成功");
+                        return response.status(200).render("jsonForm", {
+                            serverHost: config.serverHost,
+                            serverPort: config.serverPort,
+                            botList: botList,
+                            chatList: chatList,
+                            userList: userList
+                        });
+                    }).catch(function(error) {
+                        console.log("Telegram 資料查詢發生錯誤： " + error);
+                        return response.status(500).send("Telegram 資料查詢發生錯誤： " + error);
+                    });
+            })
+            .catch(function(error) { // connect failure
+                return console.log("資料庫連結發生錯誤： " + error);
+            });
     })
-    .post(jsonParser, function(request, response) { // store a message in the message queue
-        return response.status(200);
+    .post(function(request, response) { // when data is posted to the page
+        messageQueue.push({ // store a message in the message queue
+            chat_id: request.body.targetID,
+            text: request.body.message,
+            token: request.body.token
+        });
+        return response.status(200).redirect("/broadcast"); // redirect back to messaging page
     });
 
-app.listen(); // start server
+app.listen(config.serverPort); // start server
 console.log("推播系統運行中...(" + config.serverHost + ":" + config.serverPort + ")");
 
 var broadcastingSchedule = new CronJob(config.broadcastFrequency, function() { // periodically broadcast messages stored in message queue
+    console.log("目前時間： " + moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss"));
+    console.log("訊息列表");
+    messageQueue.forEach(function(message) {
+        console.log("=============================================================");
+        console.log("目標：" + message.chat_id);
+        console.log("內容：" + message.text);
+        console.log("途徑：" + message.token);
+    });
+    console.log("=============================================================");
+    if ((config.broadcastActiveStatus === true) && (messageQueue.length > 0)) { //if queue has message waiting and system is on
+        var numberOfMessageToBroadcast = (messageQueue.length >= config.broadcastQuantity) ? config.broadcastQuantity : messageQueue.length;
+        for (i = 0; i < numberOfMessageToBroadcast; i++) { // loop through from the beginning of the queue
+            httpRequest({
+                url: config.botAPIUrl + messageQueue[i].token + "/sendMessage",
+                method: "post",
+                headers: { "Content-Type": "application/json" },
+                json: {
+                    "chat_id": messageQueue[i].chat_id,
+                    "text": messageQueue[i].text,
+                    "parse_mode": "HTML"
+                }
+            }, function(error, response, body) {
+                if (error) {
+                    console.log("推播作業發生錯誤：" + error);
+                } else {
+                    console.log("推播作業成功：" + response.statusCode);
+                    console.log("伺服器回覆：" + JSON.stringify(body));
+                }
+            });
+        }
+        messageQueue = messageQueue.slice(numberOfMessageToBroadcast);
+    }
 }, null, true, config.workingLocale);
 broadcastingSchedule.start();
-
-var updateModelData = new CronJob(config.modelUpdateFreqency, function() { // periodically updates the application data model
-    console.log(
-        moment(moment(), "YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DD HH:mm:ss") +
-        " 更新推播系統相關資料");
-    mssql.connect(config.mssqlConfig).then(function() { // establish server connection
-        var mssqlRequest = new mssql.Request(); // initialize a request object
-        var queryString = "SELECT * FROM telegram.dbo.bot;";
-        mssqlRequest.query(queryString)
-            .then(function(resultset) { //query for a list of telegram bots available
-                mssql.close();
-                console.log("推播機器人列表查詢成功");
-                botList = resultset;
-            }).catch(function(error) {
-                console.log("推播機器人列表查詢失敗：" + error);
-                botList = [];
-            });
-        queryString = "SELECT * FROM telegram.dbo.chat;";
-        mssqlRequest.query(queryString)
-            .then(function(resultset) { //query for a list of chat rooms available
-                mssql.close();
-                console.log("聊天室列表查詢成功");
-                chatList = resultset;
-            }).catch(function(error) {
-                console.log("聊天室列表查詢失敗：" + error);
-                chatList = [];
-            });
-        queryString =
-            "SELECT a.*,b.NAME,b.DEP,c.NAME AS DEP_NAME,b.POS_NAME,b.ID_NO,b.CNT_TEL2 " +
-            "FROM telegram.dbo.[user] a " +
-            "LEFT JOIN DB_U105.dbo.MF_YG b ON a.erpID=b.YG_NO " +
-            "LEFT JOIN DB_U105.dbo.DEPT c ON b.DEP=c.DEP " +
-            "WHERE b.OUT_DAY IS NULL AND b.YG_NO IS NOT NULL;";
-        mssqlRequest.query(queryString)
-            .then(function(resultset) { //query for a list of users available
-                mssql.close();
-                console.log("使用者列表查詢成功");
-                userList = resultset;
-            }).catch(function(error) {
-                console.log("使用者列表查詢失敗：" + error);
-                userList = [];
-            });
-    });
-}, null, true, config.workingLocale);
-updateModelData.start();
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mssql/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
