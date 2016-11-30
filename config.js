bf242c365ffc06dc81b820a85578647c5466d548
junior-upgi <junior@upgi.com.tw>
2016-11-30 13:47:25 +0800

    minor fix

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-//var serverHost = "http://localhost"; //development
-var serverHost = "http://192.168.168.25"; // production server
+var serverHost = "http://localhost";
 var serverPort = process.env.PORT || 9001;
 //var mssqlServerHost = "http://upgi.ddns.net"; // access database from the internet (development)
 var mssqlServerHost = "http://192.168.168.5"; // access database from LAN (production)
+var mssqlServerPort = process.env.PORT || 1433;
 var upgiSystemAccount = "upgiSystem";
 var upgiSystemPassword = "upgiSystem";
 
 var mssqlConfig = {
     server: mssqlServerHost.slice(7),
     user: upgiSystemAccount,
-    password: upgiSystemPassword
+    password: upgiSystemPassword,
+    port: mssqlServerPort
 };
 
 const workingTimezone = "Asia/Taipei";
